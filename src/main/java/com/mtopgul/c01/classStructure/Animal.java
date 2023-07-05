a34d70ceaf70279a0aa68223f3c94c4a97e3592d
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-07-06 00:22:40 +0300

    > Chapter 01: Building Blocks   - Fields and Methods.

---
+++ src/main/java/com/mtopgul/c01/classStructure/Animal.java
@@ -0,0 +1,17 @@
+package com.mtopgul.c01.classStructure;
+
+/**
+ * @author muhammed-topgul
+ * @since 05/07/2023 23:53
+ */
+public class Animal {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+}
