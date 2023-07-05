71b89b50ac9413b7c96e7c353c4428839142e515
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-07-06 00:24:20 +0300

    > Chapter 01: Building Blocks   - Comments.

---
+++ src/main/java/com/mtopgul/c01/classStructure/Comments.java
@@ -0,0 +1,28 @@
+package com.mtopgul.c01.classStructure;
+
+/**
+ * @author muhammed-topgul
+ * @since 06/07/2023 00:23
+ */
+public class Comments {
+    // comment until end of the line
+
+    /* Multiple
+     * line comment
+     */
+
+    /**
+     * Javadoc multiple-line comment
+     *
+     * @author muhammed-topgul
+     */
+
+    /*
+     * // anteater
+     */
+
+    // bear
+    // // cat
+    // /* dog */
+    /* elephant */
+}
