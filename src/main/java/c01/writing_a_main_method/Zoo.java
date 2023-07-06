086e19b2e7a8bcbdf9d18197c8ab82c8527194ab
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-07-06 14:44:56 +0300

    > Chapter 01: Building Blocks   - Writing a main() Method.

---
+++ src/main/java/c01/writing_a_main_method/Zoo.java
@@ -0,0 +1,14 @@
+package c01.writing_a_main_method;
+
+/**
+ * @author muhammed-topgul
+ * @since 06/07/2023 14:06
+ */
+public class Zoo {
+    public static final void main(final String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+        System.out.println("Hello, World!");
+    }
+}
