252730bb4ae6afada4bd7cba69d7a6f8e68b1f43
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-07-08 16:30:49 +0300

    > Chapter 01: Building Blocks   - Compiling and Running Code with Packages.

---
+++ src/main/java/c01/understanding_package_declarations_and_imports/packagea/ClassA.java
@@ -0,0 +1,8 @@
+package c01.understanding_package_declarations_and_imports.packagea;
+
+/**
+ * @author muhammed-topgul
+ * @since 08/07/2023 16:06
+ */
+public class ClassA {
+}
---
+++ src/main/java/c01/understanding_package_declarations_and_imports/packageb/ClassB.java
@@ -0,0 +1,14 @@
+package c01.understanding_package_declarations_and_imports.packageb;
+
+import c01.understanding_package_declarations_and_imports.packagea.ClassA;
+
+/**
+ * @author muhammed-topgul
+ * @since 08/07/2023 16:07
+ */
+public class ClassB {
+    public static void main(String[] args) {
+        ClassA a;
+        System.out.println("Got it!");
+    }
+}
