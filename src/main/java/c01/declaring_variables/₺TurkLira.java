6c095da3709276b9275d2e1f1cd38eeb1ab702ea
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-07-13 23:16:54 +0300

    > Chapter 01: Building Blocks   - Identifying Identifiers.

---
+++ src/main/java/c01/declaring_variables/₺TurkLira.java
@@ -0,0 +1,18 @@
+package c01.declaring_variables;
+
+/**
+ * @author muhammed-topgul
+ * @since 13/07/2023 23:09
+ */
+public class ₺TurkLira {
+    long okidentifier;
+    float $Ok2Identifier;
+    boolean _alsoOK1d3ntifi3r;
+    char __SStilOk$;
+
+    // int 3DPointClass;    // identifiers cannot begin with a number
+    // byte hollywood@vine; // @ is not a letter, digit, $ or _
+    // String *coffee;      // * is not a letter, digit, $ or _
+    // double public;       // public is a reserved word
+    // short _;             // a single underscore is not allowed
+}
