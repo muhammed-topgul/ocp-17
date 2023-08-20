1ac050a8b8b7565deb7b201a7ff4bf67e1dce72e
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-08-20 19:18:09 +0300

    > Chapter 05: Methods   - Designing Methods.     - Return Type

---
+++ src/main/java/c05/designing_methods/Hike.java
@@ -0,0 +1,33 @@
+package c05.designing_methods;
+
+/**
+ * @author muhammed-topgul
+ * @since 20/08/2023 19:01
+ */
+public class Hike {
+    public void hike1() {
+    }
+
+    public void hike2() {
+    }
+
+    public String hike3() {
+        return "";
+    }
+
+    // public String hike4() {}    // DOES NOT COMPILE
+    // public hike5() {}           // DOES NOT COMPILE
+    // public String int hike() {} // DOES NOT COMPILE
+
+    // DOES NOT COMPILE
+    // String hike7(int a) {
+    //    if (1 < 2)
+    //        return "orange";
+    // }
+
+    String hike8(int a) {
+        if (1 < 2)
+            return "orange";
+        return "apple";
+    }
+}
---
+++ src/main/java/c05/designing_methods/Measurement.java
@@ -0,0 +1,22 @@
+package c05.designing_methods;
+
+/**
+ * @author muhammed-topgul
+ * @since 20/08/2023 19:08
+ */
+public class Measurement {
+    int getHeight1() {
+        int temp = 9;
+        return temp;
+    }
+    /*
+    int getHeight2() {
+        int temp = 9L; // DOES NOT COMPILE
+        return temp;
+    }
+    int getHeight3() {
+        long temp = 9L;
+        return temp; // DOES NOT COMPILE
+    }
+    */
+}
