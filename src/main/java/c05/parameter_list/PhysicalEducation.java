ad3b5db37cd354bdad45c6c02313bad863688746
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-08-20 19:25:37 +0300

    > Chapter 05: Methods   - Designing Methods.     - Parameter List

---
+++ src/main/java/c05/parameter_list/PhysicalEducation.java
@@ -0,0 +1,13 @@
+package c05.parameter_list;
+
+/**
+ * @author muhammed-topgul
+ * @since 20/08/2023 19:24
+ */
+public class PhysicalEducation {
+    public void run1() {}
+    // public void run2 {} // DOES NOT COMPILE
+    public void run3(int a) {}
+    // public void run4(int a; int b) {} // DOES NOT COMPILE
+    public void run5(int a, int b) {}
+}
