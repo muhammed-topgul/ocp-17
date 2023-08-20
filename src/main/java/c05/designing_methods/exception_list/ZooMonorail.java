2c923664c63d5ed3bcadd00cdbc7c43e131f98ea
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-08-20 19:36:08 +0300

    > Chapter 05: Methods   - Designing Methods.     - Exception List

---
+++ src/main/java/c05/designing_methods/exception_list/ZooMonorail.java
@@ -0,0 +1,16 @@
+package c05.designing_methods.exception_list;
+
+/**
+ * @author muhammed-topgul
+ * @since 20/08/2023 19:31
+ */
+public class ZooMonorail {
+    public void zeroExceptions() {
+    }
+
+    public void oneException() throws IllegalArgumentException {
+    }
+
+    public void twoExceptions() throws IllegalArgumentException, InterruptedException {
+    }
+}
