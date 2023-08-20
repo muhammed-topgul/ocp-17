4d80a45b52fec029a00482307dc966798c0529e6
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-08-20 19:23:38 +0300

    > Chapter 05: Methods   - Designing Methods.     - Parameter List

---
+++ src/main/java/c05/method_name/BeachTrip.java
@@ -0,0 +1,20 @@
+package c05.method_name;
+
+/**
+ * @author muhammed-topgul
+ * @since 20/08/2023 19:19
+ */
+public class BeachTrip {
+    public void jog1() {}
+
+    // public void 2jog() {}  // DOES NOT COMPILE
+
+    // public jog3 void() {}  // DOES NOT COMPILE
+
+    public void Jog_$() {}
+
+    // public _() {} // DOES NOT COMPILE
+
+    // public void() {} // DOES NOT COMPILE
+
+}
